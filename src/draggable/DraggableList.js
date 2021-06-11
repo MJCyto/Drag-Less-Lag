25a5bca1d9469839d4d790424336b5a9c3e58071
mj <mjkochuk@gmail.com>
2021-06-11 13:57:33 -0600

    No mid lines for when a drop would result in no change

--- src/draggable/DraggableList.js
+++ src/draggable/DraggableList.js
@@ -1,59 +1,69 @@
-import { usePreview } from 'react-dnd-preview'
-import {memo, useCallback, useState} from "react";
-import {Dustbin} from "../Dustbin";
+import { usePreview } from "react-dnd-preview";
+import { memo, useCallback, useState } from "react";
+import { Dustbin } from "../Dustbin";
 import useDimensions from "../useDimensions";
 import Box from "../Box";
 import { debounce } from "lodash";
 import DragElement from "./DragElement";
 import { v4 as uuidv4 } from "uuid";
 
-const DragPreview = (props) => {
+const DragPreview = props => {
+  const { elements, width, itemSpacing } = props;
 
-  const {elements, width, itemSpacing} = props;
-
-  const {display, item, style} = usePreview()
+  const { display, item, style } = usePreview();
   if (!display) {
-    return null
+    return null;
   }
   // console.log(style)
   // transform: "translate(332.6875px, -2px)"
 
-  const thing = style.transform.replace("translate(", "").replace(")", "").replaceAll("px", "")
-  const coordinates = thing.split(",")
+  const thing = style.transform.replace("translate(", "").replace(")", "").replaceAll("px", "");
+  const coordinates = thing.split(",");
   // const coordinates = style.transform.split("(")[1].split[","]
   // coordinates[0] = parseFloat(coordinates[0].replace("px", "")) + itemSpacing
   // coordinates[1] = parseFloat(coordinates[1].split("px")[0])
 
-  const newTransform = "translate(" + parseFloat(coordinates[0]) + itemSpacing + "px, " + coordinates[1] + "px)"
+  const newTransform =
+    "translate(" + parseFloat(coordinates[0]) + itemSpacing + "px, " + coordinates[1] + "px)";
 
   const newStyle = {
     ...style,
-    transform: newTransform
-  }
+    transform: newTransform,
+  };
   //display: "flex", alignContent: "center"
-  return (<div class="item-list__item" style={{...newStyle, width: width + "px", }}>
-    <div style={{transform: `translateY(${itemSpacing}px)`, width: "100%"}}>
-    {elements[item.index]}
+  return (
+    <div class="item-list__item" style={{ ...newStyle, width: width + "px", zIndex: 3 }}>
+      <div style={{ transform: `translateY(${itemSpacing}px)`, width: "100%" }}>
+        {elements[item.index]}
+      </div>
     </div>
-  </div>)
-}
-
+  );
+};
 
 const DraggableList = memo(function DraggableList(props) {
-  const {children, style, itemSpacing, listID} = props;
-  const [sampleRef, sampleDims] = useDimensions()
+  const { children, style, itemSpacing, listID } = props;
+  const [sampleRef, sampleDims] = useDimensions();
 
-
-  return (<div>
-    <div style={{ clear: 'both', display: "flex", flexDirection: "column", width: "100%", ...style }}>
-      {children.map((listItem, index) => (
-          <div style={{margin: -itemSpacing/2 + "px 0", width: "100%"}}>
-            <DragElement index={index} itemSpacing={itemSpacing} listItem={listItem} ref={sampleRef} listID={listID}/>
+  return (
+    <div>
+      <div
+        style={{ clear: "both", display: "flex", flexDirection: "column", width: "100%", ...style }}
+      >
+        {children.map((listItem, index) => (
+          <div style={{ margin: -itemSpacing / 2 + "px 0", width: "100%" }}>
+            <DragElement
+              index={index}
+              itemSpacing={itemSpacing}
+              listItem={listItem}
+              ref={sampleRef}
+              listID={listID}
+            />
           </div>
-          ))}
+        ))}
+      </div>
+      <DragPreview elements={children} width={sampleDims.width} itemSpacing={itemSpacing} />
     </div>
-    <DragPreview elements={children} width={sampleDims.width} itemSpacing={itemSpacing}/>
-  </div>);
+  );
 });
 
-export default DraggableList
+export default DraggableList;
--- src/index.js
+++ src/index.js
@@ -1,59 +1,62 @@
+import { render } from "react-dom";
+import Example from "./example";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
+import styled from "styled-components";
 
-	import { render } from 'react-dom'
-	import Example from './example'
-	import { DndProvider } from 'react-dnd'
-	import { HTML5Backend } from 'react-dnd-html5-backend'
-	import { TouchBackend } from 'react-dnd-touch-backend'
-	import styled from "styled-components"
+import { usePreview } from "react-dnd-preview";
+import DraggableList from "./draggable/DraggableList";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { ItemTypes } from "./ItemTypes";
+//
+// const MyPreview = () => {
+// 	const {display, itemType, item, style} = usePreview()
+// 	if (!display) {
+// 		return null
+// 	}
+// 	return <div class="item-list__item" style={style}>{item.element}</div>
+// }
 
-	import { usePreview } from 'react-dnd-preview'
-	import DraggableList from "./draggable/DraggableList";
-	import {useState} from "react";
-	import { v4 as uuidv4 } from "uuid";
-	import {ItemTypes} from "./ItemTypes";
-	//
-	// const MyPreview = () => {
-	// 	const {display, itemType, item, style} = usePreview()
-	// 	if (!display) {
-	// 		return null
-	// 	}
-	// 	return <div class="item-list__item" style={style}>{item.element}</div>
-	// }
+const Item = styled.div`
+  height: 50px;
+  width: 100%;
+  background-color: green;
+`;
 
-	const Item = styled.div`
-		height: 50px;
-		width: 100%;
-		background-color: green;
-	`;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 30% 30% 30%;
+  column-gap: 10px;
+`;
 
-	const Wrapper = styled.div`
-		display: grid;
-		grid-template-columns: 30% 30% 30%;
-		column-gap: 10px;
-	`;
+function App() {
+  const [list, setList] = useState(["Hello", "Dingus", "Yes", "No", "Goodbye"]);
+  return (
+    <div className="App">
+      <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+        <Wrapper>
+          <DraggableList itemSpacing={10} listID={ItemTypes.BOX}>
+            {list.map((listItem, index) => (
+              <Item>{listItem}</Item>
+            ))}
+          </DraggableList>
+          <DraggableList itemSpacing={10} listID={ItemTypes.TWO}>
+            {list.map((listItem, index) => (
+              <Item>{listItem}</Item>
+            ))}
+          </DraggableList>
+          <DraggableList itemSpacing={10} listID={ItemTypes.THREE}>
+            {list.map((listItem, index) => (
+              <Item>{listItem}</Item>
+            ))}
+          </DraggableList>
+        </Wrapper>
+      </DndProvider>
+    </div>
+  );
+}
 
-	function App() {
-
-		const [list, setList] = useState(["Hello", "Dingus", "Yes", "No", "Goodbye"])
-		return (
-			<div className="App">
-				<DndProvider backend={TouchBackend} options={{enableMouseEvents: true}}>
-					<Wrapper>
-						<DraggableList itemSpacing={10} listID={ItemTypes.BOX}>
-							{list.map((listItem, index) => <Item>{listItem}</Item>)}
-						</DraggableList>
-						<DraggableList itemSpacing={10} listID={ItemTypes.TWO}>
-							{list.map((listItem, index) => <Item>{listItem}</Item>)}
-						</DraggableList>
-						<DraggableList itemSpacing={10} listID={ItemTypes.THREE}>
-							{list.map((listItem, index) => <Item>{listItem}</Item>)}
-						</DraggableList>
-					</Wrapper>
-
-				</DndProvider>
-			</div>
-		)
-	}
-
-	const rootElement = document.getElementById('root')
-	render(<App />, rootElement)
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
